256c6c8a1c79dc371cbba33deafd9b40c702e201
vallecbg <valioplayofficial@gmail.com>
2020-01-20 22:01:35 +0200

    Finished 4. Services demo

---
+++ Angular/Angular.io Exercises/Tour of Heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
