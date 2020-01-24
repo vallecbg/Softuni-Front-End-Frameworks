3de6227e3d6372ff2dab143f5d633a0cbb6e4dce
vallecbg <valioplayofficial@gmail.com>
2020-01-24 18:18:45 +0200

    Added demo

--- Angular/Angular.io Exercises/Tour of Heroes/angular-tour-of-heroes/src/app/hero.service.ts
+++ Angular/Angular.io Exercises/Tour of Heroes/angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,10 +16,10 @@ export class HeroService {
     private messageService: MessageService
   ) { }
   
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 }
--- Angular/Angular.io Exercises/Tour of Heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/Angular.io Exercises/Tour of Heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,25 +8,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor(
-    private heroService: HeroService
-  ) { }
-
   heroes: Hero[];
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
